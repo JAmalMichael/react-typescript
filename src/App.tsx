a019d63873530a1575a38c9fff608fd768dbef3d
JAmalMichael <eshimi090@gmail.com>
2024-09-30 17:14:03 +0100

    typescript with props

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import Greet from "./components/Greet"
+import Status from "./components/Status"
+import Oscar from "./components/Oscar"
+
+function App() {
+
+  return (
+    <>
+      <Status status="error"/>
+      <Oscar>
+        <h1>Oscar goes to Lionardo Dicarpio</h1>
+      </Oscar>
+    </>
+  )
+}
+
+export default App
